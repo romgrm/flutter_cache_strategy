a40af79e7dd5b8ca738e21dcaa664a8ead03f1cd
Romain Gréaume <r.greaume@beapp.fr>
2022-12-01 19:53:05 +0100

    feat: implements all strategies

---
+++ lib/strategies/just_async_strategy.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_cache_strategy/storage/storage.dart';
+
+import '../runners/cache_manager.dart';
+import '../runners/cache_strategy.dart';
+
+class JustAsyncStrategy extends CacheStrategy {
+  static final JustAsyncStrategy _instance = JustAsyncStrategy._internal();
+
+  factory JustAsyncStrategy() {
+    return _instance;
+  }
+
+  JustAsyncStrategy._internal();
+
+  @override
+  Future<T?> applyStrategy<T>(AsyncBloc<T> asyncBloc, String key, SerializerBloc<T> serializerBloc, int ttlValue, Storage storage) async => await invokeAsync(asyncBloc, key, storage);
+}
