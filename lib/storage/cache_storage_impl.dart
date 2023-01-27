7d341499d0e20f3dc6de14c6e458e3433c6b8fd2
Romain Gréaume <r.greaume@beapp.fr>
2023-01-27 17:05:17 +0100

    feat: impl isEncrypted in CRUD methods  + impl  setupHive() / setupEncryption()

--- lib/storage/cache_storage_impl.dart
+++ lib/storage/cache_storage_impl.dart
@@ -1,19 +1,44 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+
 import 'storage.dart';
 
 class CacheStorage implements Storage {
+  static const _secureStorage = FlutterSecureStorage();
+  static dynamic encryptionKey;
+
   CacheStorage._internal();
 
   static final CacheStorage instance = CacheStorage._internal();
 
   factory CacheStorage() {
-    Hive.initFlutter();
+    setUpHive();
     return instance;
   }
 
+  static Future<void> setUpHive() async {
+    await Hive.initFlutter();
+  }
+
+  static Future<List<int>> setUpEncryption() async {
+    encryptionKey = await _secureStorage.read(key: 'key');
+    if (encryptionKey == null) {
+      final key = Hive.generateSecureKey();
+      await _secureStorage.write(
+        key: 'key',
+        value: base64UrlEncode(key),
+      );
+    }
+    final key = await _secureStorage.read(key: 'key');
+    return encryptionKey = base64Url.decode(key!);
+  }
+
   @override
-  Future<void> clear({String? keyCache, required String boxeName}) async {
-    final box = await Hive.openBox(boxeName);
+  Future<void> clear({String? keyCache, required String boxeName, required bool isEncrypted}) async {
+    if (isEncrypted) await setUpEncryption();
+    final box = await Hive.openBox(boxeName, encryptionCipher: isEncrypted ? HiveAesCipher(encryptionKey) : null);
     if (keyCache == null) {
       await box.clear();
     } else {
@@ -26,26 +51,30 @@ class CacheStorage implements Storage {
   }
 
   @override
-  Future<void> delete(String keyCache, String boxeName) async {
-    final box = await Hive.openBox(boxeName);
+  Future<void> delete(String keyCache, String boxeName, bool isEncrypted) async {
+    if (isEncrypted) await setUpEncryption();
+    final box = await Hive.openBox(boxeName, encryptionCipher: isEncrypted ? HiveAesCipher(encryptionKey) : null);
     return box.delete(keyCache);
   }
 
   @override
-  Future<String?> read(String keyCache, String boxeName) async {
-    final box = await Hive.openBox(boxeName);
+  Future<String?> read(String keyCache, String boxeName, bool isEncrypted) async {
+    if (isEncrypted) await setUpEncryption();
+    final box = await Hive.openBox(boxeName, encryptionCipher: isEncrypted ? HiveAesCipher(encryptionKey) : null);
     return box.get(keyCache);
   }
 
   @override
-  Future<void> write(String keyCache, String value, String boxeName) async {
-    final box = await Hive.openBox(boxeName);
+  Future<void> write(String keyCache, String value, String boxeName, bool isEncrypted) async {
+    if (isEncrypted) await setUpEncryption();
+    final box = await Hive.openBox(boxeName, encryptionCipher: isEncrypted ? HiveAesCipher(encryptionKey) : null);
     return box.put(keyCache, value);
   }
 
   @override
-  Future<int> count({String? keyCache, required String boxeName}) async {
-    final box = await Hive.openBox(boxeName);
+  Future<int> count({String? keyCache, required String boxeName, required bool isEncrypted}) async {
+    if (isEncrypted) await setUpEncryption();
+    final box = await Hive.openBox(boxeName, encryptionCipher: isEncrypted ? HiveAesCipher(encryptionKey) : null);
     if (keyCache == null) {
       return box.length;
     } else {
