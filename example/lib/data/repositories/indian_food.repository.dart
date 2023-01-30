7d61dc15b74915e3158c1cacb895c82007279c13
Romain Gréaume <r.greaume@beapp.fr>
2023-01-30 09:19:36 +0100

    fix: problems imports + dart linter

--- example/lib/data/repositories/indian_food.repository.dart
+++ example/lib/data/repositories/indian_food.repository.dart
@@ -1,7 +1,6 @@
 import 'package:example/data/datasource/fetch_data_impl.dart';
 import 'package:example/data/domain/meal.entity.dart';
-import 'package:example/data/dto/meal.dto.dart' as MealDto;
-import 'package:example/main.dart';
+import 'package:example/data/dto/meal.dto.dart' as meal_dto;
 
 import 'package:flutter_cache_strategy/strategies/async_or_cache_strategy.dart';
 import 'package:flutter_cache_strategy/cache_strategy_package.dart';
@@ -10,10 +9,10 @@ class IndianFoodRepository {
   final provider = FetchDataImpl();
 
   Future<List<MealEntity>> getIndianFood() async {
-    final List<MealDto.MealDto>? indianFood = await CacheStrategyPackage.instance.execute(
+    final List<meal_dto.MealDto>? indianFood = await CacheStrategyPackage.instance.execute(
       keyCache: "indianFood",
       boxeName: "BOXE NOT ENCRYPTED",
-      serializer: (p0) => MealDto.MealDto.fromData(p0),
+      serializer: (p0) => meal_dto.MealDto.fromData(p0),
       async: provider.getIndianFood(),
       strategy: AsyncOrCacheStrategy(),
     );
