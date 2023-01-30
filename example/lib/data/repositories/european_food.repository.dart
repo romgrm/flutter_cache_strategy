e571508c3092065ec1dc684e36c764e3381eb2d8
Romain Gréaume <r.greaume@beapp.fr>
2023-01-30 09:20:38 +0100

    fix: refactor import

--- example/lib/data/repositories/european_food.repository.dart
+++ example/lib/data/repositories/european_food.repository.dart
@@ -1,6 +1,6 @@
 import 'package:example/data/datasource/fetch_data_impl.dart';
 import 'package:example/data/domain/meal.entity.dart';
-import 'package:example/data/dto/meal.dto.dart' as MealDto;
+import 'package:example/data/dto/meal.dto.dart' as meal_dto;
 
 import 'package:flutter_cache_strategy/strategies/async_or_cache_strategy.dart';
 import 'package:flutter_cache_strategy/cache_strategy_package.dart';
@@ -11,18 +11,18 @@ class EuropeanFoodRepository {
   final _package = CacheStrategyPackage();
 
   Future<List<MealEntity>> getEuropeanFood() async {
-    final List<MealDto.MealDto>? italianFood = await _package.execute(
+    final List<meal_dto.MealDto>? italianFood = await _package.execute(
         keyCache: "italianFood",
         boxeName: "BOXE ENCRYPTED",
-        serializer: (p0) => MealDto.MealDto.fromData(p0),
+        serializer: (p0) => meal_dto.MealDto.fromData(p0),
         async: provider.getItalianFood(),
         strategy: AsyncOrCacheStrategy(),
         timeToLiveValue: 140000,
         isEncrypted: true);
-    final List<MealDto.MealDto>? frenchFood = await _package.execute(
-        keyCache: "frenchFood", boxeName: "BOXE ENCRYPTED", serializer: (p0) => MealDto.MealDto.fromData(p0), async: provider.getFrenchFood(), strategy: AsyncOrCacheStrategy(), isEncrypted: false);
+    final List<meal_dto.MealDto>? frenchFood = await _package.execute(
+        keyCache: "frenchFood", boxeName: "BOXE ENCRYPTED", serializer: (p0) => meal_dto.MealDto.fromData(p0), async: provider.getFrenchFood(), strategy: AsyncOrCacheStrategy(), isEncrypted: false);
 
-    List<MealDto.MealDto>? europeanFood = List.from(frenchFood ?? []);
+    List<meal_dto.MealDto>? europeanFood = List.from(frenchFood ?? []);
     europeanFood.addAll(italianFood ?? []);
 
     return List<MealEntity>.from(europeanFood.map((mealDto) => mealDto.toEntity()));
