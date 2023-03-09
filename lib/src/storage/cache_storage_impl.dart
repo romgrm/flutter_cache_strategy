0feca7e14687befdc759d3ef4a47371a85d07b8d
Romain Gréaume <r.greaume@beapp.fr>
2023-03-09 17:54:57 +0100

    feat: impl HiveInstance + CacheStorage.testing() method for Unit test + add getDirectory for check if box exist

--- lib/src/storage/cache_storage_impl.dart
+++ lib/src/storage/cache_storage_impl.dart
@@ -1,13 +1,18 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 
+import '../utils/cache_strategy_error.dart';
 import 'storage.dart';
 
 class CacheStorage implements Storage {
   static const _secureStorage = FlutterSecureStorage();
+  static late HiveInterface hiveInstance;
   static dynamic encryptionKey;
+  static late Directory appDir;
 
   CacheStorage._internal();
 
@@ -18,14 +23,20 @@ class CacheStorage implements Storage {
     return instance;
   }
 
+  CacheStorage.testing(HiveInterface hiveMock) {
+    hiveInstance = hiveMock;
+    appDir = Directory.current;
+  }
+
   static Future<void> setUpHive() async {
-    await Hive.initFlutter();
+    hiveInstance = Hive;
+    await hiveInstance.initFlutter();
   }
 
   static Future<List<int>> setUpEncryption() async {
     encryptionKey = await _secureStorage.read(key: 'key');
     if (encryptionKey == null) {
-      final key = Hive.generateSecureKey();
+      final key = hiveInstance.generateSecureKey();
       await _secureStorage.write(
         key: 'key',
         value: base64UrlEncode(key),
@@ -38,7 +49,7 @@ class CacheStorage implements Storage {
   @override
   Future<void> clear({String? keyCache, required String boxeName, required bool isEncrypted}) async {
     if (isEncrypted) await setUpEncryption();
-    final box = await Hive.openBox(boxeName, encryptionCipher: isEncrypted ? HiveAesCipher(encryptionKey) : null);
+    final box = await hiveInstance.openBox(boxeName, encryptionCipher: isEncrypted ? HiveAesCipher(encryptionKey) : null);
     if (keyCache == null) {
       await box.clear();
     } else {
@@ -52,22 +63,30 @@ class CacheStorage implements Storage {
 
   @override
   Future<String?> read(String keyCache, String boxeName, bool isEncrypted) async {
-    if (isEncrypted) await setUpEncryption();
-    final box = await Hive.openBox(boxeName, encryptionCipher: isEncrypted ? HiveAesCipher(encryptionKey) : null);
-    return box.get(keyCache);
+    appDir = await getApplicationDocumentsDirectory();
+    bool isBoxExist = await hiveInstance.boxExists(boxeName, path: appDir.path);
+    if (isBoxExist) {
+      if (isEncrypted) await setUpEncryption();
+      final box = await hiveInstance.openBox(boxeName, encryptionCipher: isEncrypted ? HiveAesCipher(encryptionKey) : null).onError((error, stackTrace) {
+        throw error!;
+      });
+      return box.get(keyCache);
+    } else {
+      throw CacheStrategyError("You must create the box before calling it");
+    }
   }
 
   @override
   Future<void> write(String keyCache, String value, String boxeName, bool isEncrypted) async {
     if (isEncrypted) await setUpEncryption();
-    final box = await Hive.openBox(boxeName, encryptionCipher: isEncrypted ? HiveAesCipher(encryptionKey) : null);
+    final box = await hiveInstance.openBox(boxeName, encryptionCipher: isEncrypted ? HiveAesCipher(encryptionKey) : null);
     return box.put(keyCache, value);
   }
 
   @override
   Future<int> count({String? keyCache, required String boxeName, required bool isEncrypted}) async {
     if (isEncrypted) await setUpEncryption();
-    final box = await Hive.openBox(boxeName, encryptionCipher: isEncrypted ? HiveAesCipher(encryptionKey) : null);
+    final box = await hiveInstance.openBox(boxeName, encryptionCipher: isEncrypted ? HiveAesCipher(encryptionKey) : null);
     if (keyCache == null) {
       return box.length;
     } else {
