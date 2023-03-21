4e299d7ce0bd0120c02a8d6e611cc29e00d0e138
Romain Gréaume <r.greaume@beapp.fr>
2023-03-21 11:29:28 +0100

    feat: update repositories

--- example/lib/data/repositories/european_food.repository.dart
+++ example/lib/data/repositories/european_food.repository.dart
@@ -1,6 +1,6 @@
 import 'package:example/data/datasource/fetch_data_impl.dart';
 import 'package:example/data/domain/meal.entity.dart';
-import 'package:example/data/dto/meal.dto.dart' as meal_dto;
+import 'package:example/data/dto/meal.dto.dart';
 import 'package:flutter_cache_strategy/cache_strategy_package.dart';
 
 class EuropeanFoodRepository {
@@ -9,25 +9,35 @@ class EuropeanFoodRepository {
   final _package = CacheStrategyPackage();
 
   Future<List<MealEntity>> getEuropeanFood() async {
-    final List<meal_dto.MealDto>? italianFood = await _package.execute(
-        keyCache: "italianFood",
-        boxeName: "BOXE ENCRYPTED",
-        serializer: (p0) => meal_dto.MealDto.fromData(p0),
-        async: provider.getItalianFood(),
-        strategy: AsyncOrCacheStrategy(),
-        timeToLiveValue: 140000,
-        isEncrypted: true);
-    final List<meal_dto.MealDto>? frenchFood = await _package.execute(
+    final List<MealDto>? italianFood = await _package
+        .execute(
+            keyCache: "italianFood",
+            boxeName: "BOXE ENCRYPTED",
+            serializer: (p0) => MealDto.fromData(p0),
+            async: provider.getItalianFood(),
+            strategy: AsyncOrCacheStrategy(),
+            timeToLiveValue: 140000,
+            isEncrypted: true)
+        .onError((error, stackTrace) {
+      throw error ?? Error();
+    });
+    final List<MealDto>? frenchFood = await _package
+        .execute(
       keyCache: "frenchFood",
       boxeName: "BOXE ENCRYPTED",
-      serializer: (p0) => meal_dto.MealDto.fromData(p0),
+      serializer: (p0) => MealDto.fromData(p0),
       async: provider.getFrenchFood(),
       strategy: AsyncOrCacheStrategy(),
       isEncrypted: false,
-    );
+    )
+        .onError((error, stackTrace) {
+      throw error ?? Error();
+    });
 
-    List<meal_dto.MealDto>? europeanFood = List.from(frenchFood ?? []);
-    europeanFood.addAll(italianFood ?? []);
+    frenchFood?.forEach((e) => e.strFlag = "assets/icons/france.png");
+    italianFood?.forEach((e) => e.strFlag = "assets/icons/italy.png");
+
+    List<MealDto>? europeanFood = [...?frenchFood, ...?italianFood];
 
     return List<MealEntity>.from(europeanFood.map((mealDto) => mealDto.toEntity()));
   }
--- example/lib/data/repositories/indian_food.repository.dart
+++ example/lib/data/repositories/indian_food.repository.dart
@@ -1,6 +1,6 @@
 import 'package:example/data/datasource/fetch_data_impl.dart';
 import 'package:example/data/domain/meal.entity.dart';
-import 'package:example/data/dto/meal.dto.dart' as meal_dto;
+import 'package:example/data/dto/meal.dto.dart';
 
 import 'package:flutter_cache_strategy/cache_strategy_package.dart';
 
@@ -8,15 +8,22 @@ class IndianFoodRepository {
   final provider = FetchDataImpl();
 
   Future<List<MealEntity>> getIndianFood() async {
-    final List<meal_dto.MealDto>? indianFood = await CacheStrategyPackage.instance.execute(
+    final List<MealDto>? indianFood = await CacheStrategyPackage.instance
+        .execute(
       keyCache: "indianFood",
       boxeName: "BOXE NOT ENCRYPTED",
-      serializer: (p0) => meal_dto.MealDto.fromData(p0),
+      serializer: (p0) => MealDto.fromData(p0),
       async: provider.getIndianFood(),
-      strategy: AsyncOrCacheStrategy(),
-    );
+      strategy: JustAsyncStrategy(),
+    )
+        .onError((error, stackTrace) {
+      throw error ?? Error();
+    });
 
     if (indianFood != null) {
+      for (var element in indianFood) {
+        element.strFlag = "assets/icons/india.png";
+      }
       return List<MealEntity>.from(indianFood.map((mealDto) => mealDto.toEntity()));
     }
     return [];
